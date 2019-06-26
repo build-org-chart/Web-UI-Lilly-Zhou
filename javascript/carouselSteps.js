a5a272798441fc3d5e9a9862072c43c61ee3f0d8
lilyhoratio <lilyhzhou@gmail.com>
2019-06-26 13:35:14 -0700

    got carousel images to display based on step clicked. Onto getting the carousel buttons working

--- javascript/carouselSteps.js
+++ javascript/carouselSteps.js
@@ -2,7 +2,6 @@ class Steps {
   constructor(step) {
     // STEP LIST ELEMENTS
     this.step = step
-    console.log(this.step)
     this.stepData = this.step.dataset.step
     console.log(this.stepData) // "1", "2", "3"
 
@@ -13,35 +12,33 @@ class Steps {
     this.carouselImages = this.carousel.querySelectorAll(`img[data-step="${this.stepData}"]`)
     console.log(this.carouselImages); // each image is now stored
 
-    this.carouselImages = Array.from(this.carouselImages).map (image => new CarouselImages(image))
+    this.carouselImages = Array.from(this.carouselImages).map(image => new CarouselImages(image))
 
     this.step.addEventListener("click", ()=> this.selectStep())
-    // this.stepDataArr = Array.from(this.step).map(step => step.dataset.step);
-    // console.log(this.stepDataArr); // Array ["1","2","3"]
-    
-    // for (let i = 0; i < this.stepDataArr.length; i++) {
-    //   this.carouselImages = this.carousel.querySelectorAll(`img[data-step="${this.stepDataArr[i]}"]`)
-    //   console.log(this.carouselImages)
-      // this.carouselImagesArr = Array.from(this.carouselImages).map(image => new CarouselImage(image)) 
   }
+
   selectStep() {
     const steps = document.querySelectorAll(".step")
     Array.from(steps).forEach(step => step.classList.remove("active-step"))
     this.step.classList.add("active-step")
 
     const carouselImages = document.querySelectorAll(".carousel img")
-    // console.log("HERE", carouselImages);
-    Array.from(carouselImages).forEach(image => image.classList.display = "block")
+    
+    Array.from(carouselImages).forEach(image => image.classList.remove("active-img"))
+    this.carouselImages.forEach(image => image.selectImage())
   }
 }
 
 class CarouselImages {
   constructor(image) {
     this.carouselImage = image
+    console.log("here");
   }
 
   selectImage() {
-    this.carouselImage.style.display = "block";
+    const images = document.querySelectorAll(".carousel img")
+    // images.
+    this.carouselImage.classList.add("active-img")
   }
 }
 
