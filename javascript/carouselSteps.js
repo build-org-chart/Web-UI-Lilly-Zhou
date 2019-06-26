1074624a17f996f68c095306e99b75f5d13d76cc
lilyhoratio <lilyhzhou@gmail.com>
2019-06-25 21:33:01 -0700

    for loop to select correct carousel image based on step

--- javascript/carouselSteps.js
+++ javascript/carouselSteps.js
@@ -1,18 +1,27 @@
 class Steps {
-    constructor(steps) {
-        this.steps = steps
-        // console.log(this.steps)
+  constructor(steps) {
+    // STEP LIST ELEMENTS
+    this.steps = steps;
+    // console.log(this.steps)
+    this.step = this.steps.querySelectorAll(".step");
+    this.stepData = Array.from(this.step).map(step => step.dataset.step);
+    console.log(this.stepData); // Array ["1","2","3"]
 
-        this.step = this.steps.querySelectorAll(".step")
-        this.stepData = Array.from(this.step).map(step => step.dataset.step)
-        console.log(this.stepData)
-
-        // CAROUSEL
-        this.carousel = this.steps.querySelector(".carousel")
-        this.leftButton = this.carousel.querySelector(".left-button")
-        this.rightButton = this.carousel.querySelector(".right-button")
-        this.carouselImages = this.carousel.querySelectorAll(".carousel img")
+    this.carousel = this.steps.querySelector(".carousel");
+    for (let i = 0; i < this.stepData.length; i++) {
+      this.carouselImages = document.querySelectorAll(`img[data-step="${this.stepData[i]}"]`)
+      console.log(this.carouselImages);
     }
+    console.log(this.lastIndex);
+
+    // CAROUSEL ELEMENTS
+
+    // this.leftButton = this.carousel.querySelector(".left-button")
+    // this.rightButton = this.carousel.querySelector(".right-button")
+    // this.carouselImages = this.carousel.querySelectorAll(".carousel img")
+  }
+
+  selectImage() {}
 }
 
-const steps = new Steps(document.querySelector(".steps"))
+const steps = new Steps(document.querySelector(".steps"));
