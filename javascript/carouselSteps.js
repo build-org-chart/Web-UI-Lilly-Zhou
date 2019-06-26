fa4868d42f61f6e9be5ec40db117b32714dee30e
lilyhoratio <lilyhzhou@gmail.com>
2019-06-26 08:27:55 -0700

    referencing dom elements in carousel and steps components

--- javascript/carouselSteps.js
+++ javascript/carouselSteps.js
@@ -4,24 +4,25 @@ class Steps {
     this.steps = steps;
     // console.log(this.steps)
     this.step = this.steps.querySelectorAll(".step");
-    this.stepData = Array.from(this.step).map(step => step.dataset.step);
-    console.log(this.stepData); // Array ["1","2","3"]
+    this.stepDataArr = Array.from(this.step).map(step => step.dataset.step);
+    console.log(this.stepDataArr); // Array ["1","2","3"]
 
     this.carousel = this.steps.querySelector(".carousel");
-    for (let i = 0; i < this.stepData.length; i++) {
-      this.carouselImages = document.querySelectorAll(`img[data-step="${this.stepData[i]}"]`)
-      console.log(this.carouselImages);
-    }
-    console.log(this.lastIndex);
-
-    // CAROUSEL ELEMENTS
-
     // this.leftButton = this.carousel.querySelector(".left-button")
     // this.rightButton = this.carousel.querySelector(".right-button")
-    // this.carouselImages = this.carousel.querySelectorAll(".carousel img")
-  }
+    
+    // how to convert this to a forEach?
+    // Loop through each 
+    for (let i = 0; i < this.stepDataArr.length; i++) {
+      this.carouselImages = this.carousel.querySelectorAll(`img[data-step="${this.stepDataArr[i]}"]`)
+      console.log(this.carouselImages)
+      // this.carouselImagesArr = Array.from(this.carouselImages).map(image => new CarouselImage(image)) 
+    }
 
-  selectImage() {}
+    // console.log(this.carouselImagesA)
+
+  }
+  
 }
 
 const steps = new Steps(document.querySelector(".steps"));
