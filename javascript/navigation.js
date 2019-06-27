0fc572c4da2e0003ec94bb66fa30b072cc4e73cb
lilyhoratio <lilyhzhou@gmail.com>
2019-06-27 15:43:14 -0700

    messing around with transitions and cleaning up code

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -1,24 +1,3 @@
-// BUTTON EFFECT
-
-let button = document.querySelector("button");
-
-let glow = () => {
-  console.log("glow!");
-  button.classList.add("btn-hover");
-  button.classList.remove("btn-leave");
-  console.log(button.classList);
-};
-
-let unglow = () => {
-  console.log("unglow!");
-  button.classList.add("btn-leave");
-  button.classList.remove("btn-hover");
-  console.log(button.classList);
-};
-
-button.addEventListener("mouseover", glow);
-button.addEventListener("mouseout", unglow);
-
 // HAMBURGER MENU ON TABLET/MOBILE
 // let menuOpen = () => {
 //     console.log("open!");
