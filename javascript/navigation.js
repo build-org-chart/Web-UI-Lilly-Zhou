4a9fbd4cee89eb8d74bd95d91340fb76094847dd
lilyhoratio <lilyhzhou@gmail.com>
2019-06-27 14:50:33 -0700

    created new class for main container to add padding to the top when menu button in navbar is opened

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -20,20 +20,22 @@ button.addEventListener("mouseover", glow);
 button.addEventListener("mouseout", unglow);
 
 // HAMBURGER MENU ON TABLET/MOBILE
-let menuOpen = () => {
-    console.log("open!");
-    menuLinks.classList.remove("menu-links-close")
-    menuLinks.classList.add("menu-links-open")
-}
+// let menuOpen = () => {
+//     console.log("open!");
+//     menuLinks.classList.remove("menu-links-close")
+//     menuLinks.classList.add("menu-links-open")
+// }
 
-let menuClose = () => {
-    console.log("closed!");
-    menuLinks.classList.remove("menu-links-open")
-    menuLinks.classList.add("menu-links-close")
-}
+// let menuClose = () => {
+//     console.log("closed!");
+//     menuLinks.classList.remove("menu-links-open")
+//     menuLinks.classList.add("menu-links-close")
+// }
 
 let menuToggle = () => {
     menuLinks.classList.toggle("menu-links-close")
+    const container = document.querySelector(".container")
+    container.classList.toggle("container-menu-adjusted")
 }
 
 const hamburgerButton = document.querySelector(".menu-button")
